aa45cfd37c1545eec0a75268b6f6c658df389389
Asad Malik <74582564+asadmalik287@users.noreply.github.com>
2021-02-11 18:21:14 +0500

    'sdsds'

---
+++ js/nav-footer.js
@@ -0,0 +1,120 @@
+document.getElementById("footer").innerHTML=` <div class=" bg-light container-fluid pt-5  mt-5 ">
+
+<div class="row justify-content-around">
+    
+    <div class="col-md-4 box-fill pr-5">
+        <div class="d-flex">
+            <img src="images/logo-8.png" alt="">
+            <h1 class="text-dark mt-2 pl-3 ewd-font">EWDTECH</h1>
+        </div>
+        <p class="mt-3 para-height text-justify">EWD Tech is a privately-owned Company based in Faisalabad, the industrial Hub of Pakistan. We provide IT Consultancy, Corporate-level Tech Support, Software Designs and Development, Web Application Solutions.  </p>
+        <p class="mt-3 para-height text-justify">E-commerce Store to complete business set-up along with visual identity and representation of Brands and complete Product services and maintenance solutions.</p>
+        <!-- <ul class="d-flex list-unstyled ">
+            <li class=" font-25   mr-4" class="" data-aos="fade-right" data-aos-delay="800" data-aos-duration="1000">
+                <a href="#" class="nav-link-design" href="index.html">HOME <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="font-25 mr-4" data-aos="fade-right" data-aos-delay="700" data-aos-duration="200">
+                <a href="#" class="nav-link-design" href="services.html">SERVIECE</a>
+            </li>
+
+            <li class=" font-25 mr-4" data-aos="fade-right" data-aos-delay="600" data-aos-duration="200">
+                <a href="#" class="nav-link-design disabled" href="portfolio.html">PORTFOLIO</a>
+            </li>
+            <li class="font-25 border-bottom-style" data-aos="fade-right" data-aos-delay="500" data-aos-duration="200">
+                <a href="#" class="nav-link-design" href="about.html">ABOUT US</a>
+            </li>
+
+
+        </ul> -->
+    </div>
+    <div class=" ">
+
+
+        <h1 class="text-dark animate__animated animate__bounce animate__delay-3s mt-2 mb-4">QUICK LINKS</h1>
+
+        <ul style="list-style-type:circle; margin-left: 10%;" class="list-unstyled para-height-40">
+            <li class="" data-aos="fade-up" data-aos-delay="1200" data-aos-duration="200">
+                <a  class="nav-link-design" href="index.html">WEB  DEVLOPMENT <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="" data-aos="fade-up" data-aos-delay="1400" data-aos-duration="200">
+                <a  class="nav-link-design" href="services.html">SERVIECE</a>
+            </li>
+
+            <li class="" data-aos="fade-up" data-aos-delay="1600" data-aos-duration="200">
+                <a  class="nav-link-design disabled" href="portfolio.html">PORTFOLIO</a>
+            </li>
+            <li class="" data-aos="fade-up" data-aos-delay="1800" data-aos-duration="200">
+                <a  class="nav-link-design" href="about.html">ABOUT US</a>
+            </li>
+            <li class="" data-aos="fade-up" data-aos-delay="2000" data-aos-duration="200">
+                <a  class="nav-link-design" href="index.html">HOME <span class="sr-only">(current)</span></a>
+            </li>
+         
+
+
+
+
+        </ul>
+    </div>
+    <div class="">
+        <h1 class="text-dark animate__animated animate__bounce animate__delay-3s mt-2 mb-4">CONTACT US</h1>
+        <ul class="list-unstyled">
+            <li class="para-height-last" data-aos="fade-up" data-aos-delay="1200" data-aos-duration="200">
+                <a  class="nav-link-design" href="index.html"><i class="fa fa-envelope "></i> support@ewdtech.com <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="para-height-last" data-aos="fade-up" data-aos-delay="1400" data-aos-duration="200">
+                <a  class="nav-link-design" href="services.html"><i class="fa fa-phone "></i>+92 333 2793279</a>
+            </li>
+
+            <li class="para-height-last" data-aos="fade-up" data-aos-delay="1600" data-aos-duration="200">
+                <a  class="nav-link-design disabled" href="#"> <i class="fa fa-skype  "></i> maqsood890</a>
+            </li>
+            <li class="para-height-last" data-aos="fade-up" data-aos-delay="1800" data-aos-duration="200">
+                <a  class="nav-link-design disabled" href="#"><i class="fa fa-map-marker"></i> 5th floor, mezan executive tower, Faisalabad</a>
+            </li>
+
+
+
+
+        </ul>
+     
+    </div>
+  
+</div>
+
+</div>
+<div class="container-fluid mt-4 ">
+<div class="row bg-footer">
+
+    <div class="col-md-12 text-center text-white font-15">
+        <p class="pt-1">Copy Right &copy; EWDTECH All Right Resevred</p>
+    </div>
+</div>
+
+</div>
+<div class="chatico d-flex">
+        <a href="skype:maqsood890?chat" class="color-skype" target="_blank"><i class="fa fa-skype text-white"></i></a>
+        <a href="https://api.whatsapp.com/send?phone=923332793279&amp;text=&amp;source=&amp;data=" class="color-whatsapp" target="_blank"><i class="fa fa-whatsapp text-white"></i></a>
+    </div>`;
+   
+   
+   
+   
+    document.getElementById("navbar").innerHTML=` <nav class=" ml-3 mr-3 navbar navbar-dark navbar-expand-lg" id="app-navbar">
+    <div class="container-fluid">
+        <img src="./images/logo-8.png">
+        <a class="navbar-brand" href="#"></a><button data-toggle="collapse" class="navbar-toggler text-info" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon  fa fa-bars text-dark"></span></button>
+        <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="nav navbar-nav ml-auto">
+                <li class="nav-item  mr-4  " role="presentation  "><a class="nav-link font-25 nav-link-design " href="index.html">Home</a></li>
+                <li class="nav-item mr-4" role="presentation "><a class="nav-link font-25 nav-link-design" href="services.html">Services</a></li>
+                <li class="nav-item mr-4" role="presentation "><a class="nav-link font-25 nav-link-design" href="portfolio.html">Portfolio</a></li>
+                <li class="nav-item mr-4 border-bottom-style" role="presentation "><a class="nav-link font-25 nav-link-design" href="blog.html">Blog</a></li>
+                <li class="nav-item mr-4" role="presentation "><a class="nav-link  font-25 nav-link-design" href="contact.html">Contact</a></li>
+                <li class="nav-item mr-4 " role="presentation ">
+                    <a class="nav-link font-25 nav-link-design" href="about.html">About US</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>`;
